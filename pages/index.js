e972483bb7190b18a6a7c0834d9a33d4c884264d
SungbinYang <ysb05222@naver.com>
2021-06-30 22:18:48 +0900

    Sanity 연결 후 데이터가져오기

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import SanityService from "../services/SanityService";
 
-export default function Home() {
+export default function Home({ home, posts }) {
+  console.log(home);
+  console.log(posts);
+
   return (
     <div className={styles.container}>
       <h1>Blog Home</h1>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const sanityService = new SanityService();
+  const home = await sanityService.getHome();
+  const posts = await sanityService.getPosts();
+
+  return {
+    props: {
+      home,
+      posts,
+    },
+  };
 }
